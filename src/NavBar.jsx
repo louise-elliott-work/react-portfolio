f7d6cafe5eb79b543939bed9ac7ab1560b962b03
Louise Elliott <louise.elliott.work@gmail.com>
2024-01-07 16:46:21 +0000

    rendering functionality set up and tests for navbar page selection

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -1,10 +1,54 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 // import "public/style.css";
 
 function NavBar() {
     return (
-        <NavBar />
+        <ul className="nav nav-tabs">
+        
+            <li className="nav-item">
+                <NavLink
+                    to="/"
+                    end
+                    className={({ isActive }) =>
+                    isActive ? 'nav-link active' : 'nav-link'
+                    }
+                >
+                Home
+                </NavLink>
+            </li>
+
+            <li className="nav-item">
+            <NavLink
+                to="projects"
+                className={({ isActive }) =>
+                isActive ? 'nav-link active' : 'nav-link'
+                }
+            >
+                Projects
+            </NavLink>
+            </li>
+
+            <li className="nav-item">
+            <NavLink
+                to="contact"
+                end
+                className={({ isActive }) =>
+                isActive ? 'nav-link active' : 'nav-link'
+                }
+            >
+            Contact
+            </NavLink>
+            </li>
+
+        </ul>
     )
 };
 
 export default NavBar;
+
+
+
+
+
+
